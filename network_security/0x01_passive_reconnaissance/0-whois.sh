8ff97a3682c6a30b4614527bd80e1eac5fa53b5f
arthur <nimrod@kali>
2024-12-10 09:54:22 +0100

    fix output file by removing last line

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F: '/Registrant/ || /Admin/ || /Tech/ {print $1 "," $2}' | head -n -1 > $1.csv
+whois $1 | awk -F: '/Registrant/ || /Admin/ || /Tech/ {gsub(/ /,"",$1); gsub(/ /,"",$2); print $1 "," $2}' | sed '$d' > $1.csv
