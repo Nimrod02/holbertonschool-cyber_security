8c04143b29ab8c8437af63c9baeedc5247bf9d65
nimrod02 <letrun.a@gmail.com>
2025-03-26 13:45:46 +0100

    fix script

--- web_application_security/0x0c_web_application_foresics/0-service.sh
+++ web_application_security/0x0c_web_application_foresics/0-service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep 'pam_unix' /var/log/auth.log | cut -d'(' -f2 | cut -d':' -f1 | sort | uniq -c | sort -nr | head -1
+grep "sshd" auth.log | tr -s ' ' '\n' | sort | uniq -c | sort -nr
